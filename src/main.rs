e6ba6bfb97dc74112be5ddf1311a4b9b1cad161a
Alexander Egorov <egoroff@gmail.com>
2025-01-28 23:13:08 +0300

    refactor: code simplified

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
+use rand::Rng;
+
 fn main() {
-    use rand::Rng;
-    const CHARSET: &[u8] = b"ABCDEF0123456789";
-    const PASSWORD_LEN: usize = 64;
-    let mut rng = rand::rng();
-
-    let password: String = (0..PASSWORD_LEN)
-        .map(|_| {
-            let idx = rng.random_range(0..CHARSET.len());
-            CHARSET[idx] as char
-        })
-        .collect();
-
-    println!("{password}");
+    let secret = rand::rng().random::<[u8; 32]>();
+    println!("{}", hex::encode_upper(secret));
 }
